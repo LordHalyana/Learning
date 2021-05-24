398601c0ea2c18232d0b7136b880a4724c8306f0
Halyana <patrickpettersen@live.no>
2021-05-24 14:34:26 +0200

    Add files via upload

---
+++ javascript/JS stalker Exercise/Stalker.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+var fullName = firstName + " " + lastName;
+alert("Nice to meet you, " + fullName);
+alert(fullName + " , " + age + " year old, that clicked the OK button");
+console.log("Also great to meet you, " + fullName + " ,age: "+ age);
